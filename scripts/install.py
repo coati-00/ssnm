3a154459d6ad948a90510525107fa37603a2744f
keldridge <keldridge@1f418930-7ff8-0310-b8ad-c653122473bc>
2005-08-02 19:12:35 +0000

    added install script
    git-svn-id: http://svn.ccnmtl.columbia.edu/ecomap/trunk@491 1f418930-7ff8-0310-b8ad-c653122473bc

---
+++ scripts/install.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+""" installs ecomap
+
+sets up the database tables and puts in default settings.
+
+expects that the database specified in the dsn has been created
+"""
+import sys
+sys.path.append(".")
+sys.path.append("..")
+import ecomap.config as config
+
+from optparse import OptionParser
+
+parser = OptionParser()
+parser.add_option("-m", "--mode", dest="mode", help="specify the mode", default="default", metavar="MODE")
+parser.add_option("-d", "--drop", dest="drop", help="drop existing tables", default=False, action="store_true")
+
+(options, args) = parser.parse_args()
+
+config.MODE = options.mode
+
+from ecomap.helpers import *
+
+if options.drop:
+    dropTables()
+
+createTables()
