38e633561634b76adf1ea320c1bd7b780ca29b08
anders <anders@1f418930-7ff8-0310-b8ad-c653122473bc>
2006-04-27 16:04:33 +0000

    making ecomap_start.py play nicely with mod_python
    git-svn-id: http://svn.ccnmtl.columbia.edu/ecomap/trunk@3152 1f418930-7ff8-0310-b8ad-c653122473bc

--- ecomap_start.py
+++ ecomap_start.py
@@ -4,13 +4,18 @@ pkg_resources.require("TurboGears")
 
 import cherrypy
 from os.path import *
-import sys
 
-from optparse import OptionParser
-parser = OptionParser()
-parser.add_option("-m","--mode", dest="mode", help="specify the mode", default="dev",metavar="MODE")
+try:
+    import sys
 
-(options, args) = parser.parse_args()
+    from optparse import OptionParser
+    parser = OptionParser()
+    parser.add_option("-m","--mode", dest="mode", help="specify the mode", default="dev",metavar="MODE")
+
+    (options, args) = parser.parse_args()
+except AttributeError:
+    # mod_python has no sys.argv accessible
+    pass
 
 from ecomap.controllers import build_controllers
 
