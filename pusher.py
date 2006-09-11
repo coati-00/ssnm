0909aef0d76681cab80c98ffbfadc649ab11a993
anders <anders@1f418930-7ff8-0310-b8ad-c653122473bc>
2006-09-11 21:04:38 +0000

    tweaking production setup
    git-svn-id: http://svn.ccnmtl.columbia.edu/ecomap/trunk@4919 1f418930-7ff8-0310-b8ad-c653122473bc

--- pusher.py
+++ pusher.py
@@ -7,7 +7,8 @@ def run_unit_tests(pusher):
 def post_rsync(pusher):
     """ restart apache2 """
     (out,err) = pusher.execute(["ssh","frink.ccnmtl.columbia.edu","/var/www/ecomap/init.sh","/var/www/ecomap/"])
-    (out2,err2) = pusher.execute(["ssh","frink.ccnmtl.columbia.edu","sudo","/usr/local/bin/supervisorctl","restart","ecomap"])
+    (out2,err2) = pusher.execute(["ssh","frink.ccnmtl.columbia.edu","sudo","/usr/local/bin/supervisorctl","restart","ssnm"])
+    (out2,err2) = pusher.execute(["ssh","frink.ccnmtl.columbia.edu","sudo","/usr/local/bin/supervisorctl","restart","hope"])    
     out += out2
     err += err2
     return (True,out,err)  
