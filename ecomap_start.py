c6276c72a8798d0215f6036c34e435f306d6c8b1
anders <anders@1f418930-7ff8-0310-b8ad-c653122473bc>
2006-09-11 20:11:25 +0000

    fixed config update order on production
    git-svn-id: http://svn.ccnmtl.columbia.edu/ecomap/trunk@4903 1f418930-7ff8-0310-b8ad-c653122473bc

--- ecomap_start.py
+++ ecomap_start.py
@@ -35,11 +35,11 @@ def mp_setup():
     '''
     mpcp.py looks for this method for CherryPy configs but our *.cfg files handle that.
     '''
-
+    cherrypy.config.update(file=join(dirname(__file__),"prod.cfg"))
     host = os.environ.get('ECOMAP_HOST',"ssnm")
     static = ["css","images","flash","js"]
     for s in static:
         cherrypy.config.update({"/%s" % s : {"staticFilter.on" : True, "staticFilter.dir" : "static-%s/%s" % (host,s)}})
 
-    cherrypy.config.update(file=join(dirname(__file__),"prod-hope.cfg"))
+
 
