5fbb249d6f7ec34ad850a8f9b16ccd49819773f0
anders <anders@1f418930-7ff8-0310-b8ad-c653122473bc>
2007-10-03 17:52:57 +0000

    need the mx lib too
    git-svn-id: http://svn.ccnmtl.columbia.edu/ecomap/trunk@7885 1f418930-7ff8-0310-b8ad-c653122473bc

--- pusher.py
+++ pusher.py
@@ -8,6 +8,7 @@ def post_rsync(pusher):
     """ restart apache2 """
     (out,err) = pusher.execute(["ssh","monty.ccnmtl.columbia.edu","/bin/rm","/var/www/ecomap/eggs/psycopg2-2.0.6-py2.5-linux-x86_64.egg"])
     (out,err) = pusher.execute(["ssh","monty.ccnmtl.columbia.edu","/var/www/ecomap/init.sh","/var/www/ecomap/"])
+    (out2,err2) = pusher.execute(["ssh","monty.ccnmtl.columbia.edu","/bin/ln","-s","/usr/lib/python2.5/site-packages/mx/","/var/www/ecomap/working-env/lib/python2.5/"])
     (out2,err2) = pusher.execute(["ssh","monty.ccnmtl.columbia.edu","/bin/ln","-s","/usr/lib/python2.5/site-packages/psycopg2/","/var/www/ecomap/working-env/lib/python2.5/"])
     (out2,err2) = pusher.execute(["ssh","monty.ccnmtl.columbia.edu","sudo","/usr/bin/supervisorctl","restart","ssnm"])
     (out2,err2) = pusher.execute(["ssh","monty.ccnmtl.columbia.edu","sudo","/usr/bin/supervisorctl","restart","hope"])    
