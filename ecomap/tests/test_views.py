7921066c76b3ca3abc7a63b323ae4b6322e4a7e8
Cassandra Dunlop <cdunlop@kodos.ccnmtl.columbia.edu>
2013-09-16 13:11:40 -0400

    more tests added

--- ecomap/tests/test_views.py
+++ ecomap/tests/test_views.py
@@ -33,16 +33,19 @@ class ViewTest(TestCase):  # unittest.
         '''Test that requesting about page returns a response.'''
         response = self.client.get('/ecomap/about/')
         self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed('about.html')
 
     def test_help(self):
         '''Test that requesting help page returns a response.'''
         response = self.client.get('/ecomap/help/')
         self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed('help.html')
 
     def test_contact(self):
         '''Test that requesting contact page returns a response.'''
         response = self.client.get('/ecomap/contact/')
         self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed('contact.html')
 
     def test_create_account(self):
         '''Test that user who creates account get appropriate response.'''
