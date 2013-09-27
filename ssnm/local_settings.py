f9f8c93f3d258f5bc6ca410d051cf8923b2fa526
Cassandra Dunlop <cdunlop@kodos.ccnmtl.columbia.edu>
2013-09-27 16:30:35 -0400

    changed template again

---
+++ ssnm/local_settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ssnm_4',
+        'HOST': '',
+        'PORT': 5432,
+        'USER': 'cdunlop',
+        'PASSWORD': '',
+    }
+}
