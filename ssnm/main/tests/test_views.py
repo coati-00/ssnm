29c829219f82f11fa7e9148ec30e645f10aa8e2e
Cassandra Dunlop <cdunlop@kodos.ccnmtl.columbia.edu>
2013-10-11 14:55:17 -0400

    added some of the tests back

--- ssnm/main/tests/test_views.py
+++ ssnm/main/tests/test_views.py
@@ -11,18 +11,16 @@ from django import forms
 
 
 
-class ViewTest(TestCase):  # unittest.
+class ViewTest(TestCase): 
 
     def setUp(self):
         '''Set up method for testing views.'''
         self.factory = RequestFactory()
         self.user = User.objects.create_user('somestudent', 'email@email.com', 'somestudent')
         self.user.save()
-        #IF BELOW VIEWS ARE COMMENTED OUT ALMOST EVERYTHING PASSES
         self.ecomap = Ecomap(pk='6', name="Test Map 1", ecomap_xml="<data><response>OK</response><isreadonly>false</isreadonly><name>somestudent</name><flashData><circles><circle><radius>499</radius></circle><circle><radius>350</radius></circle><circle><radius>200</radius></circle></circles><supportLevels><supportLevel><text>VeryHelpful</text></supportLevel><supportLevel><text>SomewhatHelpful</text></supportLevel><supportLevel><text>NotSoHelpful</text></supportLevel></supportLevels><supportTypes><supportType><text>Social</text></supportType><supportType><text>Advice</text></supportType><supportType><text>Empathy</text></supportType><supportType><text>Practical</text></supportType></supportTypes><persons><person><name>green</name><supportLevel>2</supportLevel><supportTypes><support>Advice</support><support>Social</support></supportTypes><x>293</x><y>70</y></person><person><name>yellow</name><supportLevel>1</supportLevel><supportTypes><support>Social</support><support>Empathy</support></supportTypes><x>448</x><y>208</y></person><person><name>red</name><supportLevel>0</supportLevel><supportTypes><support>Social</support><support>Practical</support></supportTypes><x>550</x><y>81.95</y></person></persons></flashData></data>")
         self.ecomap.owner = self.user
         self.ecomap.save()
-        #unauthenticated user
         self.bad_user = User.objects.create_user('not_ecouser', 'email@email.com', 'not_ecouser')
         self.bad_user.save()
 
@@ -127,37 +125,37 @@ class ViewTest(TestCase):  # unittest.
     #     response = get_map_details(request, 6)
     #     self.assertEqual(response.status_code, 200)
 
-    # def test_details_empty_form(self):
-    #     '''Test that user who creates account get appropriate response.'''
-    #     request = self.factory.get('/details/6/')
-    #     request.user = self.user
-    #     response = get_map_details(request, 6)
-    #     self.assertEqual(response.status_code, 200)
+    def test_details_empty_form(self):
+        '''Test that user who creates account get appropriate response.'''
+        request = self.factory.get('/details/6/')
+        request.user = self.user
+        response = get_map_details(request, 6)
+        self.assertEqual(response.status_code, 200)
 
 
-    # def test_show_maps(self):
-    #     '''Test that logged in user recieves response of home page..'''
-    #     request = self.factory.post('/show_maps/')
-    #     request.user = self.user
-    #     response = show_maps(request)
-    #     self.assertEqual(response.status_code, 200)
+    def test_show_maps(self):
+        '''Test that logged in user recieves response of home page..'''
+        request = self.factory.post('/show_maps/')
+        request.user = self.user
+        response = show_maps(request)
+        self.assertEqual(response.status_code, 200)
 
 
     # #  TEST RETRIEVAL OF SAVE MAP
-    # def test_saved_ecomap(self):
-    #     '''Test that requesting saved_ecomap page returns a response.'''
-    #     request = self.factory.post('/ecomap/6/')
-    #     request.user = self.user
-    #     response = get_map(request, 6)
-    #     self.assertEqual(response.status_code, 200)
+    def test_saved_ecomap(self):
+        '''Test that requesting saved_ecomap page returns a response.'''
+        request = self.factory.post('/ecomap/6/')
+        request.user = self.user
+        response = get_map(request, 6)
+        self.assertEqual(response.status_code, 200)
 
 
-    # def test_delete_map(self):
-    #     request = self.factory.post('/delete_map/6/')
-    #     request.user = self.user
-    #     response = delete_map(request, 6)
-    #     with self.assertRaises(Ecomap.DoesNotExist):
-    #         ecomap = Ecomap.objects.get(pk=6)
+    def test_delete_map(self):
+        request = self.factory.post('/delete_map/6/')
+        request.user = self.user
+        response = delete_map(request, 6)
+        with self.assertRaises(Ecomap.DoesNotExist):
+            ecomap = Ecomap.objects.get(pk=6)
 
 
     def test_logout(self):
@@ -169,22 +167,22 @@ class ViewTest(TestCase):  # unittest.
 
 
 # # TEST FLASH IS RETURNING RESPONSE
-#     def test_flash_ecomap(self):
-#         '''Test that requesting ecomap_page's flash conduit
-#         returns a response.'''
-#         request = self.factory.post('/ecomap/display/flashConduit')
-#         request.user = self.user
-#         response = show_maps(request)
-#         self.assertEqual(response.status_code, 200)
+    def test_flash_ecomap(self):
+        '''Test that requesting ecomap_page's flash conduit
+        returns a response.'''
+        request = self.factory.post('/ecomap/display/flashConduit')
+        request.user = self.user
+        response = show_maps(request)
+        self.assertEqual(response.status_code, 200)
 
 
-    # def test_saved_flash_ecomap(self):
-    #     '''Test that requesting saved_ecomap_page's flash conduit
-    #     returns a response.'''
-    #     request = self.factory.post('/ecomap/6/display/flashConduit')
-    #     request.user = self.user
-    #     response = show_maps(request)
-    #     self.assertEqual(response.status_code, 200)
+    def test_saved_flash_ecomap(self):
+        '''Test that requesting saved_ecomap_page's flash conduit
+        returns a response.'''
+        request = self.factory.post('/ecomap/6/display/flashConduit')
+        request.user = self.user
+        response = show_maps(request)
+        self.assertEqual(response.status_code, 200)
 
 
     def test_saved_flash(self):
