bf480e5ad3f93fdfd7db6a9969f5cde092209879
Anders Pearson <anders@columbia.edu>
2014-02-10 13:50:07 -0500

    new sentry, new raven

--- ssnm/settings_staging.py
+++ ssnm/settings_staging.py
@@ -27,22 +27,10 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 STAGING_ENV = True
 
-STATSD_PREFIX = 'ssnm-staging'
-SENTRY_SITE = 'ssnm-staging'
-SENTRY_SERVERS = ['http://sentry.ccnmtl.columbia.edu/sentry/store/']
-
 if 'migrate' not in sys.argv:
-    INSTALLED_APPS.append('raven.contrib.django')
+    INSTALLED_APPS.append('raven.contrib.django.raven_compat')
 
-    import logging
-    from raven.contrib.django.handlers import SentryHandler
-    logger = logging.getLogger()
-    # ensure we havent already registered the handler
-    if SentryHandler not in map(type, logger.handlers):
-        logger.addHandler(SentryHandler())
-        logger = logging.getLogger('sentry.errors')
-        logger.propagate = False
-        logger.addHandler(logging.StreamHandler())
+STATSD_PREFIX = 'ssnm-staging'
 
 try:
     from local_settings import *
